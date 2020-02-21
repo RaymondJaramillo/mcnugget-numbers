c52c86c6368718d63640c06197825034b5f76e25
raymondjaramillo <rjaramillo155@cnm.edu>
2020-02-21 12:04:20 -0700

    Creating isGeneralMcNugget extra credit problem

--- src/edu/cnm/deepdive/Frobenius.java
+++ src/edu/cnm/deepdive/Frobenius.java
@@ -12,4 +12,15 @@ public class Frobenius {
         || isMcNugget(value - 6)
     );
   }
+
+  /**
+   * Return true if the stated value can be formed as a sum of non-negative integral multiples of
+   * the elements of packSizes
+   * @param value target/goal number
+   * @param packSizes array of distinct, positive pack sizes, in descending order.
+   * @return true if value is a McNugger nuber using the specific pock sizes, false otherwise.
+   */
+  public static boolean isGeneralMcNugget(int value, int[] packSizes) {
+    return false; // TODO Complete implementation for extra credit.
+  }
 }
